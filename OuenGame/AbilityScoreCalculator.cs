ded7fdcc39d9ddc03335ddfcfae95fe12fb5b965
MIXA8 <javonsher@list.ru>
2022-07-20 11:51:13 +0500

    Добавьте файлы проекта.

---
+++ OuenGame/AbilityScoreCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OuenGame
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            double divided = RollResult / DivideBy;
+
+            int added = AddAmount + (int)divided;
+
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+
+        public static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [ " + lastUsedValue + " ]: ");
+            string line = Console.ReadLine();
+            if(int.TryParse(line,out int value))
+            {
+                Console.WriteLine("Используя значение " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Используя значение по умолчанию " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        public static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [ " + lastUsedValue + " ]: ");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine("Используя значение " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Используя значение по умолчанию " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+    }
+}
---
+++ OuenGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OuenGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = AbilityScoreCalculator.ReadInt(calculator.RollResult, "Начальный бросок 4d6");
+                calculator.DivideBy = AbilityScoreCalculator.ReadDouble(calculator.DivideBy, "Разделить на");
+                calculator.AddAmount = AbilityScoreCalculator.ReadInt(calculator.AddAmount, "Добавить сумму");
+                calculator.Minimum = AbilityScoreCalculator.ReadInt(calculator.Minimum, "Минимум");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Расчетная оценка способности: " + calculator.Score);
+                Console.WriteLine("Нажмите Q, чтобы выйти, любую другую клавишу, чтобы продолжить");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (keyChar == 'Q' || keyChar == 'q') return;
+            }
+        }
+    }
+}
